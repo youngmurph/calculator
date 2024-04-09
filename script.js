c81e9ce6354e22df702527e275088f2bdd652a89
Conor Murphy <conormurphy@gmx.de>
2024-04-08 21:18:55 -0400

    Update button press and operate functions

--- script.js
+++ script.js
@@ -14,16 +14,37 @@ function divide(x, y) {
     return x / y;
 }
 
-let firstNum;
-let secondNum;
-let operator = document.getElementById('operator-button');
+const display = document.getElementById('display');
+const numbers = document.getElementsByClassName('number-button');
+const button = document.querySelector('button');
+const clearAll = document.getElementById('clear-button');
+const defaultVal = display.textContent;
+
+function clear() {
+    clearAll.addEventListener('click', () => {
+        display.textContent = '0';
+    })
+}
+
+document.querySelector('#numbers').addEventListener('click', () => {
+    display.innerHTML = numbers.innerText;
+})
+
+let firstNum = {};
+let secondNum = {};
+let operators = {
+    plus : document.getElementById('plus-button'),
+    minus : document.getElementById('minus-button'),
+    multiplication : document.getElementById('multiply-button'),
+    division : document.getElementById('divide-button'),
+};
 
 function operate(operator, firstNum, secondNum) {
-    if (operator === '+') {
+    if (operator === plus) {
         add(firstNum, secondNum)
-    } else if (operator === '-') {
+    } else if (operator === minus) {
         subtract(firstNum, secondNum)
-    } else if (operator === '*') {
+    } else if (operator === multiplication) {
         multiply(firstNum, secondNum)
     } else {
         divide(firstNum, secondNum)
